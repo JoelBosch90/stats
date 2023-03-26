cef85ee9b27d99997c2d96f52f1ece1696f87596
Joël Bosch <yopc@joelbosch.nl>
2023-03-26 21:27:56 +0200

    Updated the hashing algorithm so that it's no longer deprecated

--- collector/source/store_latest_logs/hash/hash.cpp
+++ collector/source/store_latest_logs/hash/hash.cpp
@@ -1,17 +1,36 @@
 #include "hash.h"
-#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 using namespace std;
 
 string hash(string string_to_hash, string salt)
 {
-  unsigned char hash[SHA256_DIGEST_LENGTH];
-  SHA256_CTX context;
+  // Get a pointer to the SHA256 digest algorithm.
+  const EVP_MD *message_digest = EVP_sha256();
 
-  SHA256_Init(&context);
-  SHA256_Update(&context, string_to_hash.c_str(), string_to_hash.length());
-  SHA256_Update(&context, salt.c_str(), salt.length());
-  SHA256_Final(hash, &context);
+  // Allocate a buffer to hold the hash output.
+  unsigned char hash[EVP_MAX_MD_SIZE];
 
-  string hashed_string((char *)hash, SHA256_DIGEST_LENGTH);
+  // The length of the hash output.
+  unsigned int hash_length;
+
+  // Create a new message digest context object.
+  EVP_MD_CTX *context = EVP_MD_CTX_new();
+
+  // Initialize the context with the digest algorithm.
+  EVP_DigestInit_ex(context, message_digest, nullptr);
+
+  // Update the context with the input data and salt.
+  EVP_DigestUpdate(context, string_to_hash.c_str(), string_to_hash.length());
+  EVP_DigestUpdate(context, salt.c_str(), salt.length());
+
+  // Finalize the hash computation and store the output in the hash buffer.
+  EVP_DigestFinal_ex(context, hash, &hash_length);
+
+  // Free the message digest context object.
+  EVP_MD_CTX_free(context);
+
+  // Convert the hash buffer to a string and return it.
+  string hashed_string((char *)hash, hash_length);
   return hashed_string;
 };
