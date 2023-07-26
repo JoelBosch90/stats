596bd9ed1bf021a20bb15f6dcbf9a0e8a56ab937
Joël Bosch <yopc@joelbosch.nl>
2023-07-26 20:18:04 +0200

    Added API base path

--- api/src/main/java/com/stats/api/AuthenticationController.java
+++ api/src/main/java/com/stats/api/AuthenticationController.java
@@ -1,12 +1,12 @@
 package com.stats.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
 
-  @RequestMapping("/api/authenticate")
+  @GetMapping("/authenticate")
   public String authenticate() {
     return "Hello world!";
   }
